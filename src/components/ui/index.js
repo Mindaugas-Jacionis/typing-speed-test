7c7415b9ae4e05e7685dfe77a3f76ee1c789e1aa
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2017-10-19 17:16:41 +0300

    Adds blinking cursor, working timer & basic result(no word validation)

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,9 +1,11 @@
 import Button from './Button';
 import Input from './Input';
 import TextArea from './TextArea';
+import Timer from './Timer';
 
 export {
   Button,
   Input,
-  TextArea
+  TextArea,
+  Timer,
 };
